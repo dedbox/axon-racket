4907e9d9cbfc136c5b0bb86afefa616b5b0a1341
Eric Griffis <dedbox@gmail.com>
2017-12-12 10:52:36 -0800

    make net2/data API use strings only

--- axon/net2/data.rkt
+++ axon/net2/data.rkt
@@ -1,66 +1,155 @@
 #lang racket/base
 
 (require racket/contract/base
-         racket/contract/combinator
+         racket/format
          racket/list
          racket/match
-         racket/port
          racket/string
          racket/tcp
          racket/unix-socket)
 
 (provide
  (contract-out
-  [struct ip4 ((bytes (bytes/c 4)))]
-  [struct ip6 ((bytes (bytes/c 16)))]
+  ;; IP Addresses
+  [ip4 (-> string? ip4?)]
+  [ip6 (-> string? ip6?)]
+  ;; Registered Names
   [struct reg-name ()]
-  [struct dns ((labels (listof dns-label?)))]
-  [dns-label? (-> any/c boolean?)]
-  [dns-root-label dns-label?]
-  [example. dns?]
-  [example.com. dns?]
-  [example.net. dns?]
-  [example.org. dns?]
-  [invalid. dns?]
-  [local. dns?]
-  [localhost. dns?]
-  [test. dns?]
+  [dns (-> string? dns?)]
   [struct unix-socket-name ((path (and/c unix-socket-path? complete-path?)))]
+  ;; Authorities
   [authority (-> string? authority?)]
   [authority? predicate/c]
   [authority-host (-> authority? (or/c ip6? ip4? reg-name?))]
   [authority-port (-> authority? port-number?)]
+  ;; URIs
   [uri (-> string? uri?)]
   [uri? predicate/c]
   [uri-scheme (-> uri? (or/c string? #f))]
   [uri-authority (-> uri? (or/c authority? #f))]
   [uri-path (-> uri? (or/c string? #f))]
   [uri-query (-> uri? (or/c string? #f))]
-  [uri-fragment (-> uri? (or/c string #f))]))
+  [uri-fragment (-> uri? (or/c string? #f))]
+  ;; Contract Utilities
+  [bytes/c (-> exact-nonnegative-integer? flat-contract?)]))
 
-;; non-API utilities
-(provide
- (contract-out
-  [bytes->dns-label (-> bytes? dns-label?)]
-  [dns-label->bytes (-> dns-label? bytes?)]
-  [string->dns (-> string? dns?)]
-  [dns->string (-> dns? string?)]))
+(define (make-printer tag serialize)
+  (λ (obj out-port mode)
+    (define str (serialize obj))
+    (when mode
+      (write-string "(" out-port)
+      (write-string tag out-port)
+      (write-string " " out-port))
+    (cond [(number? mode) (print str out-port mode)]
+          [(not mode) (display str out-port)]
+          [else (write str out-port)])
+    (when mode
+      (write-string ")" out-port))))
 
 ;;; IP Addresses
 
-(struct ip4 (bytes) #:transparent)
-(struct ip6 (bytes) #:transparent)
+;; IPv4
+
+(struct ip4 (bytes)
+        #:transparent #:omit-define-syntaxes #:constructor-name make-ip4
+        #:methods gen:custom-write
+        [(define write-proc (make-printer "ip4" (λ (ip) (ip4->string ip))))])
+
+(define (ip4 str)
+  (define pat #px"^([^.]+)\\.([^.]+)\\.([^.]+)\\.([^.]+)$")
+  (define octets (cdr (or (regexp-match pat str) (raise 'IP4-SYNTAX))))
+  (make-ip4 (apply bytes (map string->number octets))))
+
+(define (ip4->string ip)
+  (string-join (map number->string (bytes->list (ip4-bytes ip))) "."))
+
+;; IPv6
+
+(struct ip6
+  (bytes)
+  #:transparent #:omit-define-syntaxes #:constructor-name make-ip6
+  #:methods gen:custom-write
+  [(define write-proc (make-printer "ip6" (λ (ip) (ip6->string ip))))])
+
+;; TODO add IPv4-Embedded IPv6 Addresses
+(define (ip6 str)
+  (define sec (string-split str "::" #:trim? #f))
+  (define lhs (string-split (car sec) ":"))
+  (define rhs (if (null? (cdr sec)) null (string-split (cadr sec) ":")))
+  (define (word->bytes word)
+    (define num (string->number word 16))
+    (define msb (bitwise-and num #xFF00))
+    (define lsb (bitwise-and num #x00FF))
+    (list (arithmetic-shift msb -8) lsb))
+  (define lhs-bs (map word->bytes lhs))
+  (define rhs-bs (map word->bytes rhs))
+  (define mid-bs (make-list (- 8 (+ (length lhs) (length rhs))) (list 0 0)))
+  (make-ip6 (apply bytes (flatten (list lhs-bs mid-bs rhs-bs)))))
+
+;; Ripped from RFC 5952,
+;;
+;; 4. A Recommendation for IPv6 Text Representation
+;;
+;; - suppress leading zeros in each and every field
+;; - shorten as much as possible with "::"
+;; - never shorten one field with "::"
+;; - first sequence always wins a tie with "::"
+;; - use lowercase letters for hex digits
+
+(define (ip6->string ip)
+  (define bs (ip6-bytes ip))
+  (define parts
+    (for/list ([i 8])
+      (define k (* i 2))
+      (define msb (bytes-ref bs k))
+      (define lsb (bytes-ref bs (+ k 1)))
+      (define part (bitwise-ior (arithmetic-shift msb 8) lsb))
+      (number->string part 16)))
+
+  (define (count-zeros L)
+    (cond [(null? L) 0]
+          [(equal? (car L) "0") (+ 1 (count-zeros (cdr L)))]
+          [else 0]))
+
+  (define (most-zeros L)
+    (cond [(null? L) 0]
+          [(not (equal? (car L) "0")) (most-zeros (cdr L))]
+          [else
+           (let ([head-count (count-zeros L)]
+                 [tail-count (most-zeros (cdr L))])
+             (max head-count tail-count))]))
+
+  (define (shortened L k [at-front? #t])
+    (define run (count-zeros L))
+    (define len (length L))
+    (define head (take L run))
+    (define tail (drop L run))
+    (define at-back? (null? tail))
+    (cond [(< k 2) L]
+          [(= run 8) (list ":" "")]
+          [(= run 0) (cons (car L) (shortened (cdr L) k #f))]
+          [(= run k) (cons (if (or at-front? at-back?) ":" "") tail)]
+          [else (append head (shortened tail k #f))]))
+
+  (string-join (shortened parts (most-zeros parts)) ":"))
 
 ;;; Abstract Registered Names
 
 (struct reg-name () #:transparent)
 
+(define reg-name->string void)
+
 ;;; DNS Names
 
-(struct dns reg-name (labels) #:transparent)
+(struct dns reg-name (host)
+        #:transparent #:omit-define-syntaxes #:constructor-name make-dns
+        #:methods gen:custom-write
+        [(define write-proc (make-printer "dns" (λ (d) (dns-host d))))])
 
-(define (dns-label? v)
-  (and (bytes? v) (<= (bytes-length v) 63)))
+(define (dns str)
+  (if (> (string-utf-8-length str) 255)
+      (raise 'DNS-TOO-LONG)
+      (make-dns str)))
 
 ;; Notes on RFC 1034, section 3.1:
 ;;
@@ -74,42 +163,14 @@
 ;; - to print, drop length bytes and join labels by "."
 ;; - max domain name length is 255 bytes
 
-(define dns-root-label #"")
-
-(define (bytes->dns-label bs)
-  (define len (bytes-length bs))
-  (when (> len 63)
-    (raise 'DNS-LABEL-TOO-LONG))
-  (bytes->immutable-bytes (list->bytes (cons len (bytes->list bs)))))
-
-(define (dns-label->bytes label)
-  (if (or (= 0 (bytes-length label))
-          (= 0 (bytes-ref label 0)))
-      #""
-      (subbytes label 1)))
-
-(define string->dns-label (compose bytes->dns-label string->bytes/utf-8))
-(define dns-label->string (compose bytes->string/utf-8 dns-label->bytes))
-
-(define (string->dns str)
-  (define len (string-utf-8-length str))
-  (cond [(= len 0) (raise 'DNS-TOO-SHORT)]
-        [(> len 255) (raise 'DNS-TOO-LONG)]
-        [(not (eq? #\. (string-ref str (sub1 len))))
-         (raise 'DNS-RELATIVE-PATH)])
-  (dns (map string->dns-label (string-split str "." #:trim? #f))))
-
-(define (dns->string d)
-  (string-join (map dns-label->string (dns-labels d)) "."))
-
-(define example. (string->dns "example."))
-(define example.com. (string->dns "example.com."))
-(define example.net. (string->dns "example.net."))
-(define example.org. (string->dns "example.org."))
-(define invalid. (string->dns "invalid."))
-(define local. (string->dns "local."))
-(define localhost. (string->dns "localhost."))
-(define test. (string->dns "test."))
+(define example. (dns "example."))
+(define example.com. (dns "example.com."))
+(define example.net. (dns "example.net."))
+(define example.org. (dns "example.org."))
+(define invalid. (dns "invalid."))
+(define local. (dns "local."))
+(define localhost. (dns "localhost."))
+(define test. (dns "test."))
 
 ;;; Unix Socket Names
 
@@ -120,77 +181,24 @@
 (struct authority (host port)
         #:transparent #:omit-define-syntaxes #:constructor-name make-authority)
 
-;; Ripped from RFC 3986:
-;;
-;;      authority   = host [ ":" port ]
-;;
-;;      host        = IP-literal / IPv4address / reg-name
-;;
-;;      IP-literal  = "[" ( IPv6address / IPvFuture  ) "]"
-;;
-;;      IPvFuture   = "v" 1*HEXDIG "." 1*( unreserved / sub-delims / ":" )
-;;
-;;      IPv6address =                            6( h16 ":" ) ls32
-;;                  /                       "::" 5( h16 ":" ) ls32
-;;                  / [               h16 ] "::" 4( h16 ":" ) ls32
-;;                  / [ *1( h16 ":" ) h16 ] "::" 3( h16 ":" ) ls32
-;;                  / [ *2( h16 ":" ) h16 ] "::" 2( h16 ":" ) ls32
-;;                  / [ *3( h16 ":" ) h16 ] "::"    h16 ":"   ls32
-;;                  / [ *4( h16 ":" ) h16 ] "::"              ls32
-;;                  / [ *5( h16 ":" ) h16 ] "::"              h16
-;;                  / [ *6( h16 ":" ) h16 ] "::"
-;;
-;;      ls32        = ( h16 ":" h16 ) / IPv4address
-;;                  ; least-significant 32 bits of address
-;;
-;;      h16         = 1*4HEXDIG
-;;                  ; 16 bits of address represented in hexadecimal
-;;
-;;      IPv4address = dec-octet "." dec-octet "." dec-octet "." dec-octet
-;;
-;;      dec-octet   = DIGIT                 ; 0-9
-;;                  / %x31-39 DIGIT         ; 10-99
-;;                  / "1" 2DIGIT            ; 100-199
-;;                  / "2" %x30-34 DIGIT     ; 200-249
-;;                  / "25" %x30-35          ; 250-255
-;;
-;;      reg-name    = *( unreserved / pct-encoded / sub-delims )
-;;
-;;    unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
-;;
-;;    pct-encoded   = "%" HEXDIG HEXDIG
-;;
-;;    sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
-;;                  / "*" / "+" / "," / ";" / "="
-;;
-;;      port        = *DIGIT
-;;
-;; Ripped from RFC 2234:
-;;
-;;        ALPHA          =  %x41-5A / %x61-7A   ; A-Z / a-z
-;;
-;;        DIGIT          =  %x30-39
-;;                               ; 0-9
-;;
-;;        HEXDIG         =  DIGIT / "A" / "B" / "C" / "D" / "E" / "F"
-
 (define (tok . strs)
   (string-join (append (cons "(?:" strs) (list ")")) ""))
 
 (define ALPHA "[A-Za-z]")
 (define DIGIT "[0-9]")
-(define HEXDIG "[0-9A-F]")
+(define HEXDIG "[0-9A-Fa-f]")
 
 (define (authority str)
   (define unreserved (tok ALPHA "|" DIGIT "|[-._~]"))
   (define sub-delims "[!$&'()*+,;=]")
 
   (define IPv4address
-    (let ([dec-octet (tok DIGIT
-                          "|[1-9]" DIGIT
-                          "|1" DIGIT DIGIT
+    ;; "|" takes first match instead of longest
+    (let ([dec-octet (tok "1" DIGIT DIGIT
                           "|2[0-4]" DIGIT
-                          "|25[0-5]")])
+                          "|25[0-5]"
+                          "|[1-9]" DIGIT
+                          "|" DIGIT)])
       (tok dec-octet (tok "\\." dec-octet) "{3}")))
 
   (define reg-name
@@ -216,40 +224,24 @@
            (tok "v" HEXDIG "+\\." (tok unreserved "|" sub-delims "|:") "+")])
       (tok "\\[" (tok IPv6address "|" IPvFuture) "\\]")))
 
-  (define string->ip4
-    (match-lambda
-      [(pregexp "^([^.]+)\\.([^.]+)\\.([^.]+)\\.([^.]+)$" (list-rest _ octets))
-       (ip4 (apply bytes (map string->number octets)))]))
-
-  (define (string->ip6 host)
-    (set! host (substring host 1 (- (string-length host) 1)))
-
-    (define (nibble a)
-      (string->number a 16))
-
-    (define (byte-pair abcd)
-      (define nibbles (map string (string->list abcd)))
-      (map nibble (append (make-list (- 4 (length nibbles)) "0") nibbles)))
-
-    (define (f pre post)
-      (define mid (make-list (- 8 (+ (length pre) (length post))) ""))
-      (ip6 (apply bytes (flatten (map byte-pair (append pre mid post))))))
-
-    (match (string-split host "::" #:trim? #f)
-      [(list pre-str post-str) (f (string-split pre-str ":")
-                                  (string-split post-str ":"))]
-      [(list pre-str) (f (string-split pre-str ":") null)]))
+  (define (de-bracket str)
+    (substring str 1 (- (string-length str) 1)))
 
   (match str
     [(pregexp
       (tok "^(" IP-literal "|" IPv4address "|" reg-name ")(?::(" DIGIT "+))?")
       (list _ host port))
      (make-authority (match host
-                       [(pregexp IP-literal) (string->ip6 host)]
-                       [(pregexp IPv4address) (string->ip4 host)]
-                       [(pregexp reg-name) (string->dns host)])
+                       [(pregexp IPv4address) (ip4 host)]
+                       [(pregexp IP-literal) (ip6 (de-bracket host))]
+                       [(pregexp reg-name) (dns host)])
                      (and port (string->number port)))]
     [_ (raise 'AUTHORITY-SYNTAX)]))
+
+(define (authority->string a)
+  (define host (authority-host a))
+  (define port (authority-port a))
+  (if port (~a host ":" port) (~a host)))
 
 ;;; URIs
 
